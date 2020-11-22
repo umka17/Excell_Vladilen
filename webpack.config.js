1ae95872a2274229f9488dd42b0dac352f195d14
Umka <uxuxxr21@gmail.com>
2020-11-22 15:49:42 +0500

    add resolve properties

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
         filename: 'bundle.[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html'
