9a0fae16a19fe9f1e5aa1e329bed6cef5fc9fff4
Umka <uxuxxr21@gmail.com>
2021-02-05 13:13:26 +0500

    add resizers and decompositions those

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,20 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table_templete';
+import {resizeHandler} from '@/components/table/table.resize';
 
 export class Table extends ExcelComponent {
   constructor($root) {
     super($root, {
       name: 'Table',
+      listeners: ['mousedown'],
     });
   }
   static className = 'excel__table'
   toHTML() {
     return createTable(20);
+  }
+
+  onMousedown(event) {
+    resizeHandler(this.$root, event);
   }
 }
--- src/components/table/table_templete.js
+++ src/components/table/table_templete.js
@@ -3,21 +3,29 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
-  return `<div class="cell" tabindex="1" contenteditable ></div>`;
-}
+function toCell(_, index) {
+  return `
+<div class="cell" tabindex="1" contenteditable 
+data-col="${index}">
 
-function toColumn(col) {
-  return `<div class="column">
+</div>`;
+}
+function toColumn(col, index) {
+  return `
+    <div class="column" data-type="resizable" data-col="${index}">
     ${col}
-    <div class="col-resize"></div>
+    <div class="col-resize" data-resize="col"></div>
     </div>`;
 }
 
 function createRow(index = '', content, ) {
-  const resize = index ? `<div class="row-resize"></div>`: '';
+  const html =`
+<div class="row-resize" data-resize="row" >
+
+</div>`;
+  const resize = index ? html : '';
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
         <div class="row-info">
             ${index ? index : ''}
             ${resize}
