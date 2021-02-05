1656a33e6b7a427e608b61c5b04ae629f5a2a42a
Umka <uxuxxr21@gmail.com>
2021-02-05 13:11:46 +0500

    add methods (add style with css method,)

--- src/core/dom.js
+++ src/core/dom.js
@@ -4,16 +4,19 @@ class Dom {
     // eslint-disable-next-line max-len
     this.$el =( typeof selector === 'string' ? document.querySelector(selector) : selector);
   }
+
   html(html) {
     if (typeof html === 'string') {
       this.$el.innerHTML = html;
       return this;
     } else this.$el.outerHTML.trim();
   }
+
   clear() {
     this.html('');
     return this;
   }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el;
@@ -25,11 +28,38 @@ class Dom {
     }
     return this;
   }
+
   on(listener, callback) {
     this.$el.addEventListener(listener, callback);
   }
+
   off(listener, callback ) {
     this.$el.removeEventListener(listener, callback);
+  }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  css(styles = {}) {
+    Object
+        .entries(styles)
+        .forEach((item) =>{
+          this.$el.style[item[0]] = item[1];
+        });
+  }
+
+
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+
+  getCordinats() {
+    return this.$el.getBoundingClientRect();
   }
 }
 
